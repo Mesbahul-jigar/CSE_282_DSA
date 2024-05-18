b1fb48183313066e1d53ba1127ad339f06f309c8
Mohammad Mesbahul Alam <127547048+Mesbahul-jigar@users.noreply.github.com>
2024-05-18 16:10:48 +0600

    Update l1-prac4.cpp

--- lab_1/l1-prac4.cpp
+++ lab_1/l1-prac4.cpp
@@ -8,23 +8,21 @@ struct Distance {
     int inches;
 };
 
-Distance addDistances(const Distance& d1, const Distance& d2) {
-    Distance result;
-    result.inches = d1.inches + d2.inches;
+Distance addDistances(dis d1, dis d2){
+    dis result;
+    result.inch = d1.inch+d2.inch;
 
-    if (result.inches >= 12) {
-        result.feet = d1.feet + d2.feet + result.inches / 12;
-        result.inches %= 12;
-    } else {
-        result.feet = d1.feet + d2.feet;
+    if(result.inch>= 12){
+        result.feet = d1.feet + d2.feet + result.inch/12 ;
+        result.inch %= 12;
     }
-
+    else{
+        result.feet = d1.feet+d2.feet;
+    }
     return result;
 }
 
-void displayDistance(const Distance& d, const string& label) {
-    cout << label << ": " << d.feet << " feet " << d.inches << " inches" << endl;
-}
+
 
 int main() {
     Distance dis1, dis2;
@@ -43,9 +41,7 @@ int main() {
 
     Distance sum = addDistances(dis1, dis2);
 
-    displayDistance(dis1, "First Distance");
-    displayDistance(dis2, "Second Distance");
-    displayDistance(sum, "Sum");
+    cout<< "Sum of distance "<< sum.feet <<" Feet " << sum.inch << "Inches"
 
     return 0;
 }
